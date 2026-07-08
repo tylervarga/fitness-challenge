565d85a36c6ba68eb39a344c7fb2e1bbf10c7a93
tylervarga <tylervarga@gmail.com>
2026-07-08 07:17:45 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * It looks like: https://script.google.com/macros/s/AKfycb.../exec
  * (See SETUP-GUIDE.md, Step 2.)
  */
-var API_URL = 'PASTE-YOUR-APPS-SCRIPT-URL-HERE';
+var API_URL = 'https://script.google.com/macros/s/AKfycbzzgblO7zwmVIrVXl_JETnhx-Rrn7iSesOlIWWzC3Di5D003PFzxD-mjrpXtTOUIWLHew/exec';
